e36309d56b1bc3a2cd3fdd997bf0e469ec878d4c
Enrique Alexander Pavon Gonzalez <69366738+alexgnz-poisson@users.noreply.github.com>
2023-05-13 11:14:24 -0600

    Se finalizo curso

--- service/client-service.js
+++ service/client-service.js
@@ -1,73 +1,55 @@
-const crearNuevaLinea = (nombre,email) =>{
-    const linea = document.createElement("tr");
-    const contenido = `
-    <td class="td" data-td>
-        ${nombre}
-    </td>
-    <td>
-        ${email}
-    </td>
-    <td>
-      <ul class="table__button-control">
-        <li>
-          <a
-            href="../screens/editar_cliente.html"
-            class="simple-button simple-button--edit"
-            >Editar</a
-          >
-        </li>
-        <li>
-          <button
-            class="simple-button simple-button--delete"
-            type="button"
-          >
-            Eliminar
-          </button>
-        </li>
-      </ul>
-    </td>
-  `;
-   linea.innerHTML = contenido;
-   return linea ;
-}
-const table = document.querySelector("[data-table]")
-
 //Abrir http(metodo,url)
-
 //CRUD -Metodo HTTP
 //Create -POST
 //Read -GET
 //Update -PUT-PATCH
 //Delate -Delete
-
-
-const listaClientes = () =>{
-    const promise = new Promise((resolve,reject) =>{
-        const http = new XMLHttpRequest();
-        http.open("GET","http://localhost:3000/perfil");
-        http.send();
-        http.onload = () => {
-            const response = JSON.parse(http.response)   
-            if(http.status >= 400){
-                reject(response)
-            }else{
-                resolve(response)
-            }      
-        };
-    });
-    return promise;
-};
-
-listaClientes()
-  .then((data) => {
-    data.forEach((perfil) => {
-      const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-      table.appendChild(nuevaLinea);
-    });
-  })
-  .catch((error) => alert("Ocurrió un error"));
-
 //send se encarga de enviar la peticion 
 //onload  signica que cuando carge la respuesta , vas a ejecutar esta funcion
 
 
+//Fetch API   
+const listaClientes = () => fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
+
+const crearCliente = (nombre, email) => {
+  return fetch("http://localhost:3000/perfil", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nombre, email, id: uuid.v4() }),
+  });
+};
+
+const eliminarCliente = (id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`, {
+    method: "DELETE",
+  });
+};
+
+const detalleCliente = (id) =>{
+  return fetch(`http://localhost:3000/perfil/${id}`).then(respuesta => 
+  respuesta.json()
+  );
+};
+
+const actualizarCliente = (nombre,email,id) =>{
+  return fetch(`http://localhost:3000/perfil/${id}`,{
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+  },
+  body: JSON.stringify({nombre,email})
+})
+.then((respuesta) => respuesta)
+.catch((err) => console.log(err));
+}
+
+
+export const clientServices = {
+  listaClientes,
+  crearCliente,
+  eliminarCliente,
+  detalleCliente,
+  actualizarCliente,
+};
